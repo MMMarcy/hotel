9de5f6ff5dbfda2b7c2d6dc669a069f12c4d2e16
MMMarcy <steiner.marcello@gmail.com>
2014-12-24 18:34:37 +0100

    Modified entries for the price list

--- app/models/PriceObject.scala
+++ app/models/PriceObject.scala
@@ -1,8 +1,9 @@
 package models
 
 import org.joda.time.MonthDay
-import play.api.cache.Cache
 import play.api.Play.current
+import play.api.cache.Cache
+import play.api.i18n.{Lang, Messages}
 
 object RoomType extends Enumeration {
   type RoomType = Value
@@ -16,70 +17,71 @@ object Treatment extends Enumeration {
 
 object PriceObject {
 
-  import RoomType._
-  import Treatment._
-
-  val PRICESTRING = "formattedPrices"
+  import models.RoomType._
+  import models.Treatment._
 
   val A = Period((new MonthDay(1, 10), new MonthDay(4, 8)), (new MonthDay(10, 7), new MonthDay(12, 5)))
   val B = Period((new MonthDay(4, 28), new MonthDay(6, 2)))
   val C = Period((new MonthDay(4, 9), new MonthDay(4, 27)), (new MonthDay(12, 6), new MonthDay(12, 21)))
   val D = Period((new MonthDay(6, 3), new MonthDay(7, 6)), (new MonthDay(9, 15), new MonthDay(10, 6)))
   val E = Period((new MonthDay(7, 7), new MonthDay(9, 14)), (new MonthDay(12, 22), new MonthDay(12, 31)), (new MonthDay(1, 1), new MonthDay(1, 6)))
-  val periods = List[Period](A, B, C, D, E)
+  protected val PRICESTRING = "formattedPrices"
+  protected val roomTypes = List(lakeView, mountainView, suite, single)
+  protected val treatments = List(fb, hb, bb)
+  protected val periods = List[Period](A, B, C, D, E)
+
+
+  def printTable(roomType: RoomType)(implicit lang: Lang): String = {
+    val stringBuilder = new StringBuilder()
+    stringBuilder.append("<thead>")
+    stringBuilder.append("<tr><th></th><th>A</th><th>B</th><th>C</th><th>D</th><th>E</th></tr>")
+    stringBuilder.append("</thead>")
+    stringBuilder.append("<tbody>")
+    for (treatment <- treatments) {
+      stringBuilder.append("<tr>")
+      stringBuilder.append("<td>" + Messages("treatment." + treatment.toString) + "</td>")
+      for (period <- periods) {
+        stringBuilder.append("<td>" + map(roomType)(treatment)(period.intervals.head._1) + "&euro;</td>")
+      }
+      stringBuilder.append("</tr>")
+    }
+    stringBuilder.append("</tbody>")
+    stringBuilder.toString()
+  }
 
   def printPeriods = {
     val strbld = new StringBuilder()
-    periods.foreach{ period =>
-      strbld.append("""<div class="col-xs-2 text-center val-per">""")
-      period.intervals.foreach{ interval =>
+    periods.foreach { period =>
+      strbld.append( """<div class="col-xs-1 text-center val-per">""")
+      period.intervals.foreach { interval =>
         strbld.append(interval._1.toString("dd/MM"))
         strbld.append(" - ")
         strbld.append(interval._2.toString("dd/MM"))
-        strbld.append("""<br/>""")
+        strbld.append( """<br/>""")
       }
-      strbld.append("""</div>""")
+      strbld.append( """</div>""")
     }
     strbld.toString
   }
 
   def printRow(room: RoomType, treatment: Treatment): String = {
-    Cache.getAs[String](PRICESTRING+room.toString+treatment.toString) match {
+    Cache.getAs[String](PRICESTRING + room.toString + treatment.toString) match {
       case Some(str) => str
       case None => {
         val strbld = new StringBuilder()
-        var partialFun = map(room)(treatment)(_)
+        val partialFun = map(room)(treatment)(_)
         periods.foreach(period => {
           val day = period.intervals.head._1
-          strbld.append("""<div class="col-xs-2 val text-center">""")
+          strbld.append( """<div class="col-xs-1 val text-center">""")
           strbld.append(UtilObject.format(partialFun(day)))
           strbld.append("</div>")
         })
         val res = strbld.toString()
-        Cache.set(PRICESTRING+room.toString+treatment.toString, res, 43200)
+        Cache.set(PRICESTRING + room.toString + treatment.toString, res, 43200)
         res
       }
     }
 
-  }
-
-  def getPriceForAdult(interval: (MonthDay, MonthDay), room: RoomType, treatment: Treatment): List[Float] = {
-    @annotation.tailrec
-    def innerFun(interval: (MonthDay, MonthDay), acc: List[MonthDay]): List[MonthDay] = {
-      interval match {
-        case (d1, d2) if d1.isBefore(d2) || d1.isEqual(d2) => innerFun((d1.plusDays(1), d2), d1 :: acc)
-        case _ => acc
-      }
-    }
-    val partialFun = map(room)(treatment)(_)
-    innerFun(interval, List[MonthDay]()).map(a => partialFun(a))
-  }
-
-  def getPriceForChildren(age: Int, interval: (MonthDay, MonthDay), room: RoomType, treatment: Treatment): List[Float] = {
-    if (age <= 2) getPriceForAdult(interval, room, treatment).map(_ * 0F)
-    else if (age <= 6) getPriceForAdult(interval, room, treatment).map(_ * 0.5F)
-    else if (age <= 11) getPriceForAdult(interval, room, treatment).map(_ * 0.8F)
-    else getPriceForAdult(interval, room, treatment)
   }
 
   def map(room: RoomType)(treatment: Treatment)(day: MonthDay): Float = {
@@ -151,6 +153,25 @@ object PriceObject {
       case (d, r, t) if A.contains(d) && r == RoomType.single && t == Treatment.fb => 69.0F
     }
 
+  }
+
+  def getPriceForChildren(age: Int, interval: (MonthDay, MonthDay), room: RoomType, treatment: Treatment): List[Float] = {
+    if (age <= 2) getPriceForAdult(interval, room, treatment).map(_ * 0F)
+    else if (age <= 6) getPriceForAdult(interval, room, treatment).map(_ * 0.5F)
+    else if (age <= 11) getPriceForAdult(interval, room, treatment).map(_ * 0.8F)
+    else getPriceForAdult(interval, room, treatment)
+  }
+
+  def getPriceForAdult(interval: (MonthDay, MonthDay), room: RoomType, treatment: Treatment): List[Float] = {
+    @annotation.tailrec
+    def innerFun(interval: (MonthDay, MonthDay), acc: List[MonthDay]): List[MonthDay] = {
+      interval match {
+        case (d1, d2) if d1.isBefore(d2) || d1.isEqual(d2) => innerFun((d1.plusDays(1), d2), d1 :: acc)
+        case _ => acc
+      }
+    }
+    val partialFun = map(room)(treatment)(_)
+    innerFun(interval, List[MonthDay]()).map(a => partialFun(a))
   }
 
   case class Period(val intervals: (MonthDay, MonthDay)*) {
